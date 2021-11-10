f081d0b9f77d05cf0234f2ebdc79cc7d5b5dd25e
Victor H. Salcedo <victorsalc2@gmail.com>
2021-11-10 20:24:21 +0000

    move shopping cart to its own component

--- src/front/js/component/navbar-buttons/ShoppingCart.js
+++ src/front/js/component/navbar-buttons/ShoppingCart.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../../store/appContext";
 
 export const ShoppingCart = () => {
-    return (
-        <div>Hello</div>
-    )
-}
+	const { store, actions } = useContext(Context);
+	const [dropdown, setDropdown] = React.useState(false);
+
+	let totalCart = 0;
+	let totalItems = store.shoppingCartItems.length;
+
+	let show = "";
+	if (dropdown) {
+		show = "show";
+	}
+	return (
+		<div className="dropdown" style={{ marginRight: "200px" }}>
+			<button
+				className="nav-link dropdown-toggle btn"
+				onClick={() => {
+					setDropdown(!dropdown);
+				}}
+				type="button"
+				id="navbarDropdownMenuLink"
+				role="button"
+				data-toggle="dropdown"
+				aria-haspopup="true"
+				aria-expanded="false">
+				<i className="fas fa-shopping-cart" />
+				<span>({totalItems})</span>
+			</button>
+			<div
+				className={totalItems > 0 ? "dropdown-menu" + show : "dropdown-menu"}
+				aria-labelledby="navbarDropdownMenuLink">
+				<div>
+					<h5>Cart ({totalItems})</h5>
+				</div>
+				<div>
+					<table className="table">
+						<thead>
+							<tr>
+								<th scope="col" colSpan="2">
+									Item
+								</th>
+								<th scope="col">Price</th>
+							</tr>
+						</thead>
+						<tbody>
+							{store.shoppingCartItems.map(item => {
+								totalCart += item.price;
+								return (
+									<tr key={item.name}>
+										<td className="col-2">
+											<img src={item.image} style={{ width: "100%" }} />
+										</td>
+										<td>{item.name}</td>
+										<td>${item.price}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+						<tfoot>
+							<tr>
+								<td colSpan="2" style={{ textAlign: "right" }}>
+									<strong>TOTAL</strong>
+								</td>
+								<td>${totalCart}</td>
+							</tr>
+							<tr>
+								<td>
+									<button type="button" className="btn btn-success">
+										Checkout
+									</button>
+								</td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,10 @@ import React, { useContext } from "react";
 
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { ShoppingCart } from "./navbar-buttons/ShoppingCart";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
-	const [dropdown, setDropdown] = React.useState(false);
-
-	let totalCart = 0;
-	let totalItems = store.shoppingCartItems.length;
-
-	let show = "";
-	if (dropdown) {
-		show = "show";
-	}
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container-fluid">
@@ -25,70 +17,7 @@ export const Navbar = () => {
 						<button className="btn btn-primary">Check the Context in action</button>
 					</Link>
 				</div>
-				<div className="dropdown" style={{ marginRight: "200px" }}>
-					<button
-						className="nav-link dropdown-toggle btn"
-						onClick={() => {
-							setDropdown(!dropdown);
-						}}
-						type="button"
-						id="navbarDropdownMenuLink"
-						role="button"
-						data-toggle="dropdown"
-						aria-haspopup="true"
-						aria-expanded="false">
-						<i className="fas fa-shopping-cart" />
-						<span>({totalItems})</span>
-					</button>
-					<div
-						className={dropdown ? "dropdown-menu" + show : "dropdown-menu"}
-						aria-labelledby="navbarDropdownMenuLink">
-						<div>
-							<h5>Cart ({totalItems})</h5>
-						</div>
-						<div>
-							<table className="table">
-								<thead>
-									<tr>
-										<th scope="col" colSpan="2">
-											Item
-										</th>
-										<th scope="col">Price</th>
-									</tr>
-								</thead>
-								<tbody>
-									{store.shoppingCartItems.map(item => {
-										totalCart += item.price;
-										return (
-											<tr key={item.name}>
-												<td className="col-2">
-													<img src={item.image} style={{ width: "100%" }} />
-												</td>
-												<td>{item.name}</td>
-												<td>${item.price}</td>
-											</tr>
-										);
-									})}
-								</tbody>
-								<tfoot>
-									<tr>
-										<td colSpan="2" style={{ textAlign: "right" }}>
-											<strong>TOTAL</strong>
-										</td>
-										<td>${totalCart}</td>
-									</tr>
-									<tr>
-										<td>
-											<button type="button" className="btn btn-success">
-												Checkout
-											</button>
-										</td>
-									</tr>
-								</tfoot>
-							</table>
-						</div>
-					</div>
-				</div>
+				<ShoppingCart />
 			</div>
 		</nav>
 	);
