49028d49839ba74d94a5601a8ad7f5149026398d
Victor Salcedo <VSalcedo@ngpvan.com>
2021-11-25 00:10:22 -0500

    add more routes. First pass at adding time to db

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -3,69 +3,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			message: null,
 			//hardcoding these items here for the moment
-			items: [
-				{
-					id: 1,
-					category: "clothing",
-					saleType: "sale",
-					condition: "good",
-					itemType: "t-shirt",
-					price: 10.0,
-					endDate: "11/27/2021",
-					image: "https://cdn.shopify.com/s/files/1/0559/6715/4340/products/365354_420x525.jpg?v=1633403005"
-				},
-				{
-					id: 2,
-					category: "clothing",
-					saleType: "sale",
-					condition: "good",
-					itemType: "pants",
-					price: 15.0,
-					endDate: "11/26/2021",
-					image: "https://cdn.shopify.com/s/files/1/0559/6715/4340/products/360020_420x525.jpg?v=1636152457"
-				},
-				{
-					id: 3,
-					category: "furniture",
-					saleType: "sale",
-					condition: "good",
-					itemType: "dining table",
-					price: 20.0,
-					endDate: "11/25/2021",
-					image:
-						"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRmYIRC351z7UHKLZFAEnEEjYTv04vycQSgA&usqp=CAU"
-				},
-				{
-					id: 4,
-					category: "books",
-					saleType: "bid",
-					condition: "good",
-					itemType: "book",
-					price: 5.0,
-					endDate: "11/24/2021",
-					image:
-						"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRNWkhhnQFio8ncl4yGYNcZ-4LKmilmF76eA&usqp=CAU"
-				}
-			],
+			items: [],
 			shoppingCartItems: [],
-			nonprofits: [
-				{
-					id: 1,
-					name: "good non-profit",
-					logo: "https://bit.ly/3n6N8tC",
-					description: "Good company is focused on healthy pokemon living",
-					needs: ["clothes"],
-					totalfunds: 1000.0
-				},
-				{
-					id: 2,
-					name: "gooder non-profit",
-					logo: "https://bit.ly/3n84p5W",
-					description: "Gooder company is focused on sustainability",
-					needs: ["furniture"],
-					totalfunds: 0.0
-				}
-			],
+			nonprofits: [],
 			currentuser: {
 				id: 1,
 				name: "good non-profit",
@@ -77,17 +17,31 @@ const getState = ({ getStore, getActions, setStore }) => {
 			}
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getItems: () => {
+				fetch("https://3001-aqua-anteater-lbzo25xi.ws-us17.gitpod.io/api/items")
+					.then(response => {
+						if (!response.ok) {
+							throw new Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						console.log(data);
+						setStore({ items: data });
+					});
 			},
-
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
+			getNonprofits: () => {
+				fetch("https://3001-aqua-anteater-lbzo25xi.ws-us17.gitpod.io/api/nonprofit")
+					.then(response => {
+						if (!response.ok) {
+							throw new Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						console.log(data);
+						setStore({ nonprofits: data });
+					});
 			},
 
 			addToShoppingCart: (item, price, image) => {
@@ -100,10 +54,27 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			addAuctionItem: item => {
-				let { items } = getStore();
-				// items.push(item);
-
-				setStore({ items: [...items, item] });
+				fetch("https://3001-aqua-anteater-lbzo25xi.ws-us17.gitpod.io/api/items", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(item)
+				})
+					.then(response => {
+						if (response.ok) {
+							fetch("https://3001-aqua-anteater-lbzo25xi.ws-us17.gitpod.io/api/items")
+								.then(response => {
+									if (!response.ok) {
+										throw new Error(response.statusText);
+									}
+									return response.json();
+								})
+								.then(data => {
+									console.log(data);
+									setStore({ items: data });
+								});
+						}
+					})
+					.catch(err => console.error("Error:", err));
 			},
 
 			// arrow functions to update contacts
