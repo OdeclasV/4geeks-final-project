2255809a6e0bf4a2dda8ed9e2391c68b4287b7de
Victor Salcedo <VSalcedo@ngpvan.com>
2021-12-05 17:59:12 -0500

    add styling to homepage

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center navbar-fixed-bottom">
-		<p>Shop To Give 2021</p>
+		<p>Bid for Good 2021</p>
 	</footer>
 );
--- src/front/js/pages/NonProfitPartners.js
+++ src/front/js/pages/NonProfitPartners.js
@@ -9,20 +9,34 @@ export const NonProfitPartners = () => {
 		<div className="nonprofits d-flex justify-content-around flex-wrap">
 			{store.nonprofits.map(nonprofit => {
 				return (
-					<div className="card col-md-3 m-3" style={{ width: "18rem" }} key={nonprofit.id}>
-						<img className="card-img-top" src={nonprofit.nonprofit_logo} alt="Card image cap" />
-						<div className="card-body">
-							<h5 className="card-title">{nonprofit.name}</h5>
-							<p className="card-text item-price">{nonprofit.description}</p>
-							<p className="card-text item-price">${nonprofit.totalfunds}</p>
-							<a
-								href="#"
-								className="btn btn-primary m-1"
-								onClick={() => {
-									console.log("clicked!");
-								}}>
-								More information
-							</a>
+					// <div className="card col-md-3 m-3" style={{ width: "30rem", height: "200px" }} key={nonprofit.id}>
+					// 	<img
+					// 		className="card-img-top"
+					// 		src={nonprofit.nonprofit_logo}
+					// 		style={{ width: "15%" }}
+					// 		alt="Card image cap"
+					// 	/>
+					// 	{/* <div className="card-body">
+					// 		<h5 className="card-title">{nonprofit.name}</h5>
+					// 		<p className="card-text item-price">{nonprofit.description}</p>
+					// 	</div> */}
+					// </div>
+					<div className="card mb-3" style={{ maxWidth: "540px", height: "150px" }} key={nonprofit.id}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img
+									src={nonprofit.nonprofit_logo}
+									className="img-fluid rounded-start"
+									alt={nonprofit.name}
+									style={{ height: "100px", width: "100px", marginTop: "15px" }}
+								/>
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">{nonprofit.name}</h5>
+									<p className="card-text">{nonprofit.description}</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				);
